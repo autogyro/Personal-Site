f5cb3ebb10bc72df27480e00d383ba02f82e9d93
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-07-01 22:36:25 +0100

    template inheritance and the about page

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ manager = Manager(app)
 def index():
 	return render_template('index.html')
 
-@app.route('/portfolio')
-def portfolio():
-	return render_template('portfolio.html')
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 
 if __name__ == '__main__':
